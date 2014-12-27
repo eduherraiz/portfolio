a8652386d35ced827b321cb775d804e77e15726e
Edu Herraiz <gshark@gmail.com>
2014-12-27 18:57:30 +0100

    Final url for blog

--- src/main/urls.py
+++ src/main/urls.py
@@ -12,7 +12,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^new-blog/', include('zinnia.urls', namespace='zinnia')),
+    url(r'^blog/', include('zinnia.urls', namespace='zinnia')),
     url(r'^comments/', include('django_comments.urls')),
     url(r'^ckeditor/', include('ckeditor.urls')),
 )
