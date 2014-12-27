e3eb510cee1629d3cf0044a74b7c5c438ebce84d
Edu Herraiz <gshark@gmail.com>
2014-12-27 19:06:06 +0100

    Blog feed

--- src/home/static/js/main.js
+++ src/home/static/js/main.js
@@ -26,7 +26,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
     
         //Change this to your own rss feeds
-        "http://www.eduherraiz.com/blog/feed/",
+        "http://www.eduherraiz.com/blog/feeds/",
 
         {
         // how many entries do you want?
